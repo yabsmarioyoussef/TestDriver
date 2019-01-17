a2b03e1c87351ef619580170ec64cd3868195de9
Youssef Mario <mario.youssef@scania.com>
2019-01-17 23:49:56 +0100

    Testdriver project

---
+++ TestDriver/src/TestDriver.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "C:\Users\MYXKAB\eclipse-workspace\TestingProject\src\TestingProject.c"
+
+int main(void) {
+	testingWolfModule();
+}
+
+void testingWolfModule(void) {
+	nameTest();
+
+	ageTest();
+
+	energyTest();
+
+	eatTest();
+
+	huntTest();
+
+	poopTest();
+
+	sleepTest();
+
+	ttlTest();
+}
